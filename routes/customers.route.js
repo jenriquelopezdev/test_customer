ab0d5eee21752eec06594d5c6acc81feed87137e
Enrique Lopez <jenriquelopez.dev@gmail.com>
2020-01-09 15:44:39 -0600

    service customer

--- routes/customers.route.js
+++ routes/customers.route.js
@@ -7,7 +7,13 @@ module.exports = router;
 
 router
   .route("/")
-  .post(asyncHandler(insert));
+  .post(asyncHandler(insert))
+  .get(asyncHandler(getAll));
+router
+  .route("/:id")
+  .get(asyncHandler(getById))
+  .put(asyncHandler(updateById))
+  .delete(asyncHandler(deleteById));
 
 async function insert(req, res) {
   try {
@@ -19,5 +25,51 @@ async function insert(req, res) {
     }
   } catch (err) {
     res.boom.boomify(err);
+  }
+}
+
+async function getAll(req, res) {
+  try {
+    let customers = await customersCtrl.getCustomers();
+    res.json(customers);
+  } catch (err) {
+    res.boom.badRequest("The records was not found.");
+  }
+}
+
+async function getById(req, res) {
+  try {
+    let id = req.params.id;
+    let customers = await customersCtrl.getById(id);
+    if (customers == null) {
+      res.boom.badRequest("The record was not found.");
+    } else {
+      res.json(customers);
+    }
+  } catch (err) {
+    res.boom.badRequest("The record was not found.");
+  }
+}
+
+async function updateById(req, res) {
+  try {
+    let id = req.params.id;
+    let customers = await customersCtrl.updateById(id, req.body);
+    if (customers.error != null) {
+      res.boom.badRequest(customers.error);
+    } else {
+      res.json(customers);
+    }
+  } catch (err) {
+    res.boom.badRequest("The record was not found.");
+  }
+}
+
+async function deleteById(req, res) {
+  try {
+    let customers = await customersCtrl.deleteById(req.params.id);
+    res.json(customers);
+  } catch (err) {
+    res.boom.badRequest("The record was not found.");
   }
 }
